8c65740176972047d594fa43d73cb3c55d061119
TheLazySquid <TheLazySquid2@gmail.com>
2023-03-28 12:55:52 -0400

    Add toggling

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 All this is is an extremely simple script that automatically answers quizlet live questions for you. To run this, copy the script from [script.js](/script.js) and paste it into your browser's console. This needs to be done **before** the game, on the "study while you wait screen." It should look like this: ![image](/images/StartOnScreen.png)
 
 After that, just sit back and relax!
+
+### Toggling the script
+
+Hitting shift 3 times will toggle the script on and off. To check whether the script is on, look at the console.
--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     const questionAmount = document.querySelector(".CardsList-navControl.progressIndex .UIText").innerHTML.split("/")[1];
 
     let questions = {}
+    let enabled = true;
     
     for(let i = 0; i < questionAmount; i++){
         let question = document.querySelectorAll(".CardsItem.current .FormattedText.notranslate div")
@@ -16,12 +17,20 @@
     console.log(questions);
     
     const pageChange = () => {
+        if(!enabled) return;
         let question = document.querySelector(".StudentPrompt .FormattedText.notranslate div")?.innerHTML;
         if(!question) return;
     
         let answer = questions[question];
-        if(!answer) return;
-        console.log(answer)
+        if(!answer) {
+            // attempt to get the answer from a value
+            for(let key of Object.keys(questions)) {
+                if(questions[key] === question) {
+                    answer = key;
+                    break;
+                }
+            }
+        }
     
         const buttons = document.querySelectorAll(".UIButton .FormattedText.notranslate div");
     
@@ -36,4 +45,25 @@
     observer.observe(document.body, {childList: true, subtree: true})
 
     pageChange();
+
+    let shiftCount = 0;
+    let shiftTimeout = null;
+    document.addEventListener("keydown", (e) => {
+        if(e.key === "Shift"){
+            shiftCount++;
+            if(shiftTimeout) clearTimeout(shiftTimeout);
+            shiftTimeout = setTimeout(() => {
+                shiftCount = 0;
+            }, 1000);
+            if(shiftCount == 3) {
+                enabled = !enabled;
+                if(enabled) {
+                    console.log("Quizlet Live Cheat Enabled");
+                    pageChange();
+                }
+                else console.log("Quizlet Live Cheat Disabled");
+                shiftCount = 0;
+            }
+        }
+    })
 })();
